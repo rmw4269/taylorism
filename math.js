dfb88b195bda5f96bd6d4c0a528377ac7a29b766
rmw4269 <rmw4269@rit.edu>
2021-08-04 02:53:56 -0400

    add Math.product

--- math.js
+++ math.js
@@ -1,2 +1,9 @@
 "use strict";
 
+/**
+ * This multiplies all of the given arguments together, so you’d better hope that they’re numbers. With no arguments, one is returned;
+ *
+ * @param values values to multiply
+ * @return product of all values
+ */
+Math.product = (...values) => values.reduce((a, b) => a * b, 1);
