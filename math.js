7ae333bad217a6059f53699cbe9ead8620788ef4
rmw4269 <rmw4269@rit.edu>
2021-08-04 02:54:54 -0400

    add Math.mean & Math.median

--- math.js
+++ math.js
@@ -7,3 +7,18 @@
  * @return product of all values
  */
 Math.product = (...values) => values.reduce((a, b) => a * b, 1);
+
+/**
+ * @return mathematical mean of the given values
+ */
+Math.mean = (...values) => Math.sum(...values) / values.length;
+
+/**
+ * @return mathematical median of the given values
+ */
+Math.median = (...values) => values.length === 0
+	? undefined
+	: (values.sort((a, b) => a - b).length % 2 === 0)
+		? (values[a.length / 2 - 1] + values[a.length / 2]) / 2
+		: values[(values.length - 1) / 2];
+
